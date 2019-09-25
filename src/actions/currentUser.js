2a0c024bb6f3a8688f889f24cf1ba0c91abcb311
Minajeanne <albrown07@mac.com>
2019-09-25 16:23:29 -0400

    add clearCurrentUser & update login

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -6,16 +6,34 @@ export const setCurrentUser = user => {
   }
 }
 
-// asynchronous action creators
-export const login = credentials => {
-  return dispatch => {
+export const clearCurrentUser = () => {
+  return {
+    type: "CLEAR_CURRENT_USER"
+  }
+}
 
+// asynchronous action creators
+export const login = (credentials, history) => {
+  return dispatch => {
     return fetch("http://localhost:3001/api/v1/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({username: "QuizWhiz", password: "password"})
+      body: JSON.stringify(credentials)
     })
+    .then(r => r.json())
+      .then(response => {
+        if (response.error) {
+          alert(response.error)
+        } else {
+          dispatch(setCurrentUser(response.data))
+          // figure out what to add here **
+          // dispatch(getMyTrips())
+          dispatch(resetLoginForm())
+          history.push('/')
+        }
+      })
+      .catch(console.log)
   }
 }
