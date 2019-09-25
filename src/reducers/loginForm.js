84b39632ce1aee8d40d854cfb56422179d248514
Minajeanne <albrown07@mac.com>
2019-09-25 16:09:59 -0400

    update Login update/submission

--- src/reducers/loginForm.js
+++ src/reducers/loginForm.js
@@ -3,12 +3,11 @@ const initialState = {
   password: ""
 }
 
-
 export default (state = initialState, action) => {
   switch (action.type) {
     case 'UPDATE_LOGIN_FORM':
-      return action.user
-      
+      return action.formData;
+
     default:
       return state;
   }
