f0621ea52415f68f367bc6b359ee0aa5ac8bb943
Minajeanne <albrown07@mac.com>
2019-09-26 16:27:32 -0400

    stub out MyGames

---
+++ src/components/MyGames.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+const MyGames = props => {
+  const gameCards = props.games.length > 0 ?
+    props.games.map(t => (<p key={t.id}><Link to={`/games/${t.id}`}>{t.attributes.name}</Link></p>)) :
+    null
+
+  return gameCards
+}
+
+// we provide mapStateToProps to Redux in order to tell Redux:
+// "Excuse me Redux, would you please provide use access to your state
+// so that we may pick and choose the pieces of state we would like availble
+// to this particular component as props."
+
+const mapStateToProps = state => {
+  return {
+    games: state.myGames
+  }
+}
+
+export default connect(mapStateToProps)(MyGames)
