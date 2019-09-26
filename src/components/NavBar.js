1728d93df012b10a743dcf2fc42d97e61da74474
Minajeanne <albrown07@mac.com>
2019-09-26 14:29:20 -0400

    create NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { NavLink } from 'react-router-dom'
+import Logout from './Logout'
+
+const NavBar = ({ currentUser, loggedIn }) => {
+  return (
+    <div className="NavBar">
+      // <NavLink exact activeClassName="active" to="/games"  >My Games</NavLink>
+      // <NavLink exact activeClassName="active" to="/games/new" >New Game</NavLink>
+      { loggedIn ? <><p id="loggedin">Logged in as {currentUser.attributes.name}</p><Logout /></> : null}
+    </div>
+  )
+}
+
+const mapStateToProps = ({ currentUser }) => {
+  return {
+    currentUser,
+    loggedIn: !!currentUser
+  }
+}
+
+export default connect(mapStateToProps)(NavBar);
